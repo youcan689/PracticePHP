fcb531857a240a7bbc8174d336f1667391c77f66
Albert Chen <youcan689@gmail.com>
2021-02-23 16:57:50 +0800

    '練習laravel'

--- TestLaravel/routes/web.php
+++ TestLaravel/routes/web.php
@@ -10,10 +10,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//use Illuminate\Support\Facades\Route;
 
 Route::get('/hello', function () {
     return view('welcome');
 });
-Route::get(uri:'/about.html',function(){
+Route::get("/about.html",function(){
 	return "hi";
+});
+
+Route::get('user/{name?}', function ($name = null) {
+    return $name;
+});
+
+Route::get('user/{name?}', function ($name = 'John') {
+    return $name;
+});
+
+
+//cats/2
+//網址後要帶參數        後面也有個參數通常是同名  影片講解不錯
+Route::get('')
+
+
+
+Route::get("/cats/{id}", function($x){
+     return "我是第".$x."號的貓";
 });
